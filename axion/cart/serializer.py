84d648cdc021c36572836a7d5c1b8e55d089b63f
brandy <2023_brandy.sherile@iticbcn.cat>
2025-04-07 18:22:02 +0200

    cart

---
+++ axion/cart/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Cart, CartItem
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['cart', 'cart_item_id', 'product', 'quantity', 'price', 'subtotal']
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+
+    class Meta:
+        model = Cart
+        fields = ['cart_id', 'customer', 'items']
--- axion/cart/views.py
+++ axion/cart/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Cart, CartItem
+from .serializer import CartSerializer, CartItemSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def cart_list_create(request):
+    if request.method == 'GET':
+        carts = Cart.objects.all()
+        serializer = CartSerializer(carts, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'DELETE'])
+def cart_detail(request, cart_id):
+    try:
+        cart = Cart.objects.get(cart_id=cart_id)
+    except Cart.DoesNotExist:
+        return Response({"error": "Cart not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        cart.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST'])
+def cart_item_list_create(request):
+    if request.method == 'GET':
+        cart_items = CartItem.objects.all()
+        serializer = CartItemSerializer(cart_items, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'DELETE'])
+def cart_item_detail(request, cart_item_id):
+    try:
+        cart_item = CartItem.objects.get(cart_item_id=cart_item_id)
+    except CartItem.DoesNotExist:
+        return Response({"error": "Cart item not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CartItemSerializer(cart_item)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        cart_item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
